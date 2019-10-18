550f621e21909f8cfe27799d099e3d70bda6fbe8
jyun97 <qyun@umich.edu>
2019-10-17 20:41:50 -0400

    added homescreen module

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,30 @@
 import React from 'react'
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 
 const HomeScreen = ({ navigation }) => (
-    <View>
-        <Text>Welcome, Brenda!</Text>
-        <Button onPress={() => navigation.navigate('VisionQ1')} title="Take vision test" />
+    <View style={styles.container}>
+        <Text style={styles.heading}>Welcome, Brenda!</Text>
+  
+        <Button style={styles.buttons}onPress={() => navigation.navigate('VisionQ1')} title="Take vision test" />
+        <Button title="View previous results" />
     </View>
   )
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  heading: {
+  	fontWeight: 'bold',
+  	fontSize: 20, 
+  	textAlign: 'center',
+  	marginTop: 50
+  },
+  buttons: {
+  	marginTop: 90
+  }
+})
 
 export default HomeScreen
