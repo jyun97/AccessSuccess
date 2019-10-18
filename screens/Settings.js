8e2104a96316552c2698139852982ec4c7dffdab
jyun97 <qyun@umich.edu>
2019-10-17 20:36:28 -0400

    added settings file for accessibility info

---
+++ screens/Settings.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  SectionList,
+} from 'react-native';
+// import Constants from 'expo-constants';
+
+const DATA = [
+  {
+    title: 'Text Size Features',
+    data: ['Zoom', 'Magnifier', 'Display & Text Size'],
+  },
+  {
+    title: 'Color Features',
+    data: ['Reduce Transparency', 'Increase Contrast', 'Differentiate without Color'],
+  }
+];
+
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
+
+export default function Settings() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Item title={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+  },
+  title: {
+    fontSize: 24,
+  },
+});
