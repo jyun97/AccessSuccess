647240b0407deb3c4afdcbd6693c558f3605157d
Enya <enyac@umich.edu>
2019-11-18 12:50:52 -0500

    Removed hardcoding Brenda account

--- screens/Map.js
+++ screens/Map.js
@@ -161,7 +161,6 @@ export const mapResults = async() => {
   	}
 
     //Get username and store recommendation to user
-    storeAnswer("currentUser", "Brenda");
     const username = await AsyncStorage.getItem("currentUser");
     storeAnswer(JSON.stringify(username), JSON.stringify(rec));
     return rec;
